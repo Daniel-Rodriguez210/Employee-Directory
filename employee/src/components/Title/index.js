81dfcc325282054f136b2b53b3c5a0f4b419e774
Daniel Rodriguez <drod18@hotmail.com>
2020-11-22 20:50:20 -0600

    formatting the title

--- employee/src/components/Title/index.js
+++ employee/src/components/Title/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./style.css";
 
 function Title(props) {
-  return <h1 className="title">{props.children}</h1>;
+  return (
+    <div className="container">
+      <div className="jumbotron jumbotron-fluid">
+      <h1 className="title">{props.children}</h1>
+      </div>
+    </div>
+  )
 }
 
 export default Title;
+
+
