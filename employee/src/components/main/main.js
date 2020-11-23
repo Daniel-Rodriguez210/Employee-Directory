59b494aedb3d852eb5f8c9b514c01b6b9720b56c
Daniel Rodriguez <drod18@hotmail.com>
2020-11-22 19:44:56 -0600

    switched components to have employees on cards

--- employee/src/components/main/main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function main(props) {
-  return <div className="container" style={props.style}>{props.children}</div>;
-}
-
-export default main; 
